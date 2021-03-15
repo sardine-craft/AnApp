db849a69b1c86a09d517a0ac84b71a4e69f4834d
sardine-craft <65100242+sardine-craft@users.noreply.github.com>
2021-03-15 17:07:59 -0400

    API numbering, new css var, base link display

--- app.js
+++ app.js
@@ -3,17 +3,38 @@ fetch("https://api.publicapis.org/entries")
 .then(getTheAPIs);
 
 function getTheAPIs(arkgs) {
+    //log Agks
     console.log(arkgs);
+    //add a handle to the-stuff (APIs)
     let APIs = document.getElementById("the-stuff");
+
+    //say number of APIs
+    document.getElementById("APINumber").innerHTML = arkgs.count;
+ 
+    //declare var to keep track of APIs
+    var n = 1;
+
+    //add every API to APIs
     for (let igg of arkgs.entries) {
+        //declare variables
         let para = document.createElement("a");
         let breack = document.createElement("br");
 
+        //add a break
         APIs.appendChild(breack);
+
+        //give para attributes
         para.setAttribute("href", igg.Link);
         para.setAttribute("target", "_none");
-        para.innerHTML = igg.API + " - " + igg.Description;
+
+        //add html to para
+        para.innerHTML = n + ". " + igg.API + " - " + igg.Description + " - Base link: " + igg.Link;
+
+        //append para to APIs.
         APIs.appendChild(para);
+
+        //positively change API number
+        n++;
     }
 
 }
